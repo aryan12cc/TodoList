9f91b293d6294999454c9d4f0addd3714b857e2c
Aryan Maskara <aryanmaskara@gmail.com>
2023-12-20 04:08:34 +0530

    Added delete items function

--- public/script.js
+++ public/script.js
@@ -6,12 +6,10 @@ Todo:
 
 document.addEventListener('DOMContentLoaded', function () {
     var addItemForm = document.getElementById('addItemForm');
-    // var deleteItemForm = document.getElementById('deleteItemForm');
     var todoList = document.getElementById('todoList');
     var addItemInput = document.getElementById('addItem');
-    // var deleteItemInput = document.getElementById('deleteItem');
+    var deleteItems = document.getElementById('deleteItemsButton');
 
-    // fetches the current todo list
     fetchTodoList();
 
     addItemForm.addEventListener('submit', function (event) {
@@ -24,18 +22,15 @@ document.addEventListener('DOMContentLoaded', function () {
         updateAllItems();
     });
 
-    
-
-    // deleteItemForm.addEventListener('submit', function (event) {
-    //     event.preventDefault();
-    //     deleteItem();
-    // });
+    deleteItems.addEventListener('click', function (event) {
+        event.preventDefault();
+        deleteCheckedItems();
+    });
 
     function addItem() {
         var addItemText = addItemInput.value.trim();
 
         if (addItemText !== '') {
-            // node request to add a new item to the to-do list
             fetch('/api/AddItem', {
                 method: 'POST',
                 headers: {
@@ -46,10 +41,8 @@ document.addEventListener('DOMContentLoaded', function () {
                 .then(response => response.json())
                 .then(data => {
                     if (data.success) {
-                        // If the item is added successfully, update the UI
                         fetchTodoList();
                         addItemInput.value = '';
-                        // console.log('Added item with: ', addItemText);
                     } else {
                         console.error('Failed to add item:', data.message);
                     }
@@ -59,22 +52,17 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     function updateAllItems() {
-        // console.log("Coming to updateItem() with todoList = ", todoList);
         const todoListItems = document.querySelectorAll('#todoList li');
-        // console.log("todoList items = ", todoListItems);
         const itemList = Array.from(todoListItems).map(item => item.textContent);
-        // console.log("item list = ", itemList);
 
 
         const checkboxes = todoList.querySelectorAll('input[type="checkbox"]');
 
         const checkBoxList = Array.from(checkboxes).map(item => item.checked);
 
-        // console.log("checkboxes = ", checkBoxList);
         checkboxes.forEach(checkbox => {
             const itemId = checkbox.id;
             const isChecked = checkbox.checked;
-            // console.log(`Item ${itemId}: ${isChecked}`);
         });
 
 
@@ -88,7 +76,6 @@ document.addEventListener('DOMContentLoaded', function () {
             .then(response => response.json())
             .then(data => {
                 if (data.success) {
-                    // If the checkbox is updated successfully, update the UI
                     fetchTodoList();
                 } else {
                     console.error('Failed to add item:', data.message);
@@ -97,7 +84,36 @@ document.addEventListener('DOMContentLoaded', function () {
             .catch(error => console.error(error));
     }
 
-    // function deleteItem() {
+    function deleteCheckedItems() {
+        const todoListItems = document.querySelectorAll('#todoList li');
+        const itemList = Array.from(todoListItems).map(item => item.textContent);
+
+
+        const checkboxes = todoList.querySelectorAll('input[type="checkbox"]');
+
+        const checkBoxList = Array.from(checkboxes).map(item => item.checked);
+
+        checkboxes.forEach(checkbox => {
+            const itemId = checkbox.id;
+            const isChecked = checkbox.checked;
+        });
+
+        fetch('/api/DeleteCheckedItems', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ list: itemList, checkbox: checkBoxList}),
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    fetchTodoList();
+                } else {
+                    console.error('Failed to add item:', data.message);
+                }
+            })
+            .catch(error => console.error(error));
     //     var deleteItemText = deleteItemInput.value.trim();
 
     //     if(deleteItemText !== '') {
@@ -121,15 +137,12 @@ document.addEventListener('DOMContentLoaded', function () {
     //             })
     //             .catch(error => console.error('Error'))
     //     }
-    // }
+    }
 
-    // Function to fetch the to-do list from the server
     function fetchTodoList() {
         fetch('/api/TodoList')
             .then(response => response.json())
             .then(data => {
-                // Update the UI with the fetched to-do list
-                // console.log("script.js line 132: The data I have got from fetching the todo list is", data);
                 renderTodoList(data);
             })
             .catch(error => console.error(error));
@@ -138,20 +151,11 @@ document.addEventListener('DOMContentLoaded', function () {
     // Function to render the to-do list in the UI
     function renderTodoList(items) {
 
-        // console.log("In renderTodoList");
-        // items.forEach(item => {
-        //     console.log(item);
-        // });
-
-        // console.log("the data i have to render is", items);
-
         var actualList = []
         for(let i = 0; i < items.length; i++)
         {
             actualList.push(items[i].content);
         }
-
-        // console.log("actual list is", actualList);
 
         todoList.innerHTML = '';
         for(let i = 0; i < actualList.length; i++)
@@ -161,49 +165,20 @@ document.addEventListener('DOMContentLoaded', function () {
             var label = document.createElement('label');
             label.className = 'checkbox-container';
 
-            // Create the checkbox input
             var checkbox = document.createElement('input');
             checkbox.type = 'checkbox';
             checkbox.checked = items[i].isChecked;
 
-            // Create the checkmark span for styling
             var checkmark = document.createElement('span');
             checkmark.className = 'checkmark';
 
-            // Append the checkbox and checkmark to the label
             label.appendChild(checkbox);
             label.appendChild(checkmark);
 
             label.appendChild(document.createTextNode(actualList[i]));
 
-            // Append the label to the list item
             newItem.appendChild(label);
             todoList.appendChild(newItem);
         }
-        // console.log(todoList);
-        // actualList.forEach(item => {
-        //     var newItem = document.createElement('li');
-
-        //     var label = document.createElement('label');
-        //     label.className = 'checkbox-container';
-
-        //     // Create the checkbox input
-        //     var checkbox = document.createElement('input');
-        //     checkbox.type = 'checkbox';
-
-        //     // Create the checkmark span for styling
-        //     var checkmark = document.createElement('span');
-        //     checkmark.className = 'checkmark';
-
-        //     // Append the checkbox and checkmark to the label
-        //     label.appendChild(checkbox);
-        //     label.appendChild(checkmark);
-
-        //     label.appendChild(document.createTextNode(item));
-
-        //     // Append the label to the list item
-        //     newItem.appendChild(label);
-        //     todoList.appendChild(newItem);
-        // });
     }
 });
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const { todo } = require('node:test');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Use middleware to parse JSON and urlencoded request bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files (HTML, CSS, JS)
 app.use(express.static(path.join(__dirname, 'public')));
 
 const item = {
@@ -17,23 +16,12 @@ const item = {
     isChecked: false
 };
 
-// In-memory data storage
 let todoList = [];
 
-// API endpoint to get all the contents in the to-do list
 app.get('/api/TodoList', (req, res) => {
     res.json(todoList);
-
-    // let todoListReply = [];
-    // console.log("Coming in /api/TodoList");
-    // todoList.forEach(function(currentItem) {
-    //     todoListReply.push(currentItem.content);
-    //     console.log("current Item = ", currentItem.content, ", ", currentItem.isChecked);
-    // });
-    // res.json(todoListReply);
 });
 
-// API endpoint to add a new item to the to-do list
 app.post('/api/AddItem', (req, res) => {
     const newItem = {
         ...item, 
@@ -45,33 +33,33 @@ app.post('/api/AddItem', (req, res) => {
 });
 
 app.post('/api/UpdateAllItems', (req, res) => {
-    // console.log("All requests: ", req.body);
     const currentTodoList = req.body.list;
     const currentCheckmarkList = req.body.checkbox;
-    // console.log("The current todo list is", currentTodoList);
-    // console.log("The current checkmark list is", currentCheckmarkList);
     todoList = [];
     for(let i = 0; i < currentTodoList.length; i++)
     {
         let item = {content: currentTodoList[i], isChecked: currentCheckmarkList[i]};
         todoList.push(item);
     }
-    // console.log("New todo list: ", todoList);
     res.json({ success: true, message: 'Todo List updated successfully.'});
 });
 
-// app.post('/api/deleteitem', (req, res) => {
-//     const currentItem = req.body.newItem;
-//     if(todoList.includes(currentItem) == false) {
-//         res.json({ success: false, message: 'Item could not be found in the list.'});
-//     }
-//     else {
-//         todoList = todoList.filter(element => element !== currentItem)
-//         res.json({ success: true, message: 'Item deleted successfully.'})
-//     }
-// });
+app.post('/api/DeleteCheckedItems', (req, res) => {
+    const currentTodoList = req.body.list;
+    const currentCheckmarkList = req.body.checkbox;
+    todoList = []
+    for(let i = 0; i < currentCheckmarkList.length; i++)
+    {
+        if(currentCheckmarkList[i] == true)
+        {
+            continue;
+        }
+        let item = {content: currentTodoList[i], isChecked: currentCheckmarkList[i]};
+        todoList.push(item);
+    }
+    res.json({ success: true, message: 'Todo List updated successfully.'});
+});
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
