15f0d38d2f73217f3c7067022981280c1d3e40b8
Aryan Maskara <aryanmaskara@gmail.com>
2023-12-29 20:41:50 +0530

    Revert "Revert "Revert "Create data base and separate list for each user"""
    This reverts commit 402ee127aad4a530ccd71a5f686045332ded0722.

--- server.js
+++ server.js
@@ -11,18 +11,15 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-const DataBase = 'dataBase.txt';
-
 const item = {
     content: '',
     isChecked: false
 };
 
-let userTodoList = [];
-let userTodoListFile = '';
+let todoList = [];
 
 app.get('/api/TodoList', (req, res) => {
-    res.json(userTodoList);
+    res.json(todoList);
 });
 
 function operateTodoList(req, operation) {
@@ -32,42 +29,37 @@ function operateTodoList(req, operation) {
             content: req.body.newItem, 
             isChecked: false
         };
-        userTodoList.push(newItem);
-        writeTodoListToFile(userTodoListFile);
+        todoList.push(newItem);
+        writeToFile('savedTodoList.txt');
         return;
     }
     const currentTodoList = req.body.list;
     const currentCheckmarkList = req.body.checkbox;
-    userTodoList = [];
+    todoList = [];
     for(let i = 0; i < currentTodoList.length; i++) {
         if(operation == 'delete' && currentCheckmarkList[i] == true) {
             continue;
         }
         let item = {content: currentTodoList[i], isChecked: currentCheckmarkList[i]};
-        userTodoList.push(item);
+        todoList.push(item);
     }
-    writeTodoListToFile(userTodoListFile);
+    writeToFile('savedTodoList.txt');
 }
 
 function readFromFile(filePath) {
     fs.readFile(filePath, 'utf8', (err, data) => {
         if(err) {
-            const contentToWrite = '[\n\n]';
-            data = contentToWrite;
-            fs.writeFile(filePath, contentToWrite, 'utf8', (writeErr) => {
-                if (writeErr) {
-                    console.error('Error creating file:', writeErr);
-                }
-            });
+            console.error(err);
+            return;
         }
         const parsedData = JSON.parse(data);
-        userTodoList = parsedData;
-        return parsedData;
+        todoList = parsedData;
+        return data;
     });
 }
 
-function writeTodoListToFile(filePath) {
-    const todoListAsJson = JSON.stringify(userTodoList, null, 2);
+function writeToFile(filePath) {
+    const todoListAsJson = JSON.stringify(todoList, null, 2);
     fs.writeFile(filePath, todoListAsJson, 'utf8', (err) => {
         if(err) {
             console.error(err);
@@ -76,90 +68,9 @@ function writeTodoListToFile(filePath) {
     });
 }
 
-function searchFile(filePath, username, password, checkPasssword) {
-    try {
-        const data = fs.readFileSync(filePath, 'utf8');
-
-        const allUsers = data.split('\n');
-
-        for(const currentUser of allUsers) {
-            const [currentUsername, currentPassword] = currentUser.split(':');
-            if(username === currentUsername && password === currentPassword) {
-                return true;
-            }
-            if(username === currentUsername && !checkPasssword) {
-                return true;
-            }
-        }
-        return false;
-    }
-    catch {
-        console.error('Error reading file:', err);
-    }
-}
-
-function appendToFile(filePath, data) {
-    fs.appendFile(filePath, data, 'utf8', (err) => {
-        if (err) {
-            console.error('Error appending to file:', err);
-        }
-    });
-}
-
-app.post('/api/LoginUser', (req, res) => {
-    let result = searchFile(DataBase, req.body.username, req.body.password, true);
-    if(result == true) {
-        userTodoListFile = req.body.username + '.txt';
-        userTodoList = readFromFile(userTodoListFile);
-        res.json({success: true, message: 'Logged in successfully.'});
-    }
-    else {
-        res.json({success: false, message: 'Incorrect username/password.'});
-    }
-});
-
-app.post('/api/RegisterUser', (req, res) => {
-    let result = searchFile(DataBase, req.body.username, '', false);
-    if(result == true) {
-        res.json({success: false, message: 'Username already taken.'});
-        return;
-    }
-    const password = req.body.password;
-    if(password.length < 8) {
-        res.json({success: false, message: 'Password must be of atleast 8 characters.'});
-        return;
-    }
-    let number = false, special = false, capital = false;
-    req.body.password.split('').forEach(function(currentCharacter) {
-        if(currentCharacter >= '0' && currentCharacter <= '9') {
-            number = true;
-        }
-        else if(currentCharacter >= 'A' && currentCharacter <= 'Z') {
-            capital = true;
-        }
-        else if(currentCharacter < 'a' || currentCharacter > 'z') {
-            special = true;
-        }
-    });
-    if(number == false) {
-        res.json({success: false, message: 'Password must contain a number.'});
-        return;
-    }
-    if(capital == false) {
-        res.json({success: false, message: 'Password must contain a capital letter.'});
-        return;
-    }
-    if(special == false) {
-        res.json({success: false, message: 'Password must contain a special character.'});
-        return;
-    }
-    const data = req.body.username + ':' + req.body.password + '\n';
-    appendToFile(DataBase, data);
-    res.json({success: true, message: 'Username added. Please login.'});
-});
-
 app.post('/api/AddItem', (req, res) => {
     operateTodoList(req, 'add');
+    console.log(todoList);
     res.json({ success: true, message: 'Item added successfully.' });
 });
 
@@ -175,4 +86,5 @@ app.post('/api/DeleteCheckedItems', (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+    readFromFile('savedTodoList.txt');
 });
