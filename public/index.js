26b7684ee9e02ce690e0214a9edd255e86fa912e
Aryan Maskara <aryanmaskara@gmail.com>
2023-12-29 20:33:22 +0530

    Create data base and separate list for each user

--- public/index.js
+++ public/index.js
@@ -1,7 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var loginFormId = document.getElementById('loginForm');
-    var registerFormId = document.getElementById('registerForm');
-    var switchFormId = document.querySelector('.switch');
+    let loginFormId = document.getElementById('loginForm');
+    let registerFormId = document.getElementById('registerForm');
+    let switchFormId = document.querySelector('.switch');
+
+    let errorMessageShown = false;
 
     loginFormId.addEventListener('submit', function(event) {
         event.preventDefault();
@@ -20,20 +22,78 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
 
+    function showLoginErrorMessage(message) {
+        if(errorMessageShown === true) return;
+        let errorDiv = document.createElement('invalid-username');
+        errorDiv.style.color = '#ed4337';
+        let errorText = document.createTextNode(message);
+        errorDiv.appendChild(errorText);
+        console.log(errorText);
+        loginFormId.appendChild(errorDiv);
+    }
+
     function loginForm() {
-        console.log('coming to login form');
-        
+        let loginFormUsername = document.getElementById('login-username').value;
+        let loginFormPassword = document.getElementById('login-password').value;
+
+        if(loginFormUsername !== '' && loginFormPassword !== '') {
+            fetch('/api/LoginUser', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username: loginFormUsername, password: loginFormPassword}),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        // let errorDiv = document.getElementById('invalid-username');
+                        // errorDiv.remove();
+                        errorMessageShown = false;
+                        window.location.href = 'todolist.html';
+                    } else {
+                        showLoginErrorMessage(data.message);
+                        errorMessageShown = true;
+                    }
+                })
+                .catch(error => console.error(error));
+        }
     }
 
     function registerForm() {
         console.log('coming to register form');
+        let registerFormUsername = document.getElementById('register-username').value;
+        let registerFormPassword = document.getElementById('register-password').value;
+
+        console.log('username:', registerFormUsername);
+        console.log('password:', registerFormPassword);
+
+        if(registerFormUsername !== '' && registerFormPassword !== '') {
+            fetch('/api/RegisterUser', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ username: registerFormUsername, password: registerFormPassword}),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        console.log('User successfully registered!');
+                        switchForm();
+                    } else {
+                        console.log('Failed to register user:', data.message);
+                    }
+                })
+                .catch(error => console.error(error));
+        }
     }
 
     function switchForm() {
         loginFormId.style.display = loginFormId.style.display === 'block' ? 'none' : 'block';
         registerFormId.style.display = registerFormId.style.display === 'block' ? 'none' : 'block';
 
-        var content = document.querySelector('.switch p').innerHTML;
+        let content = document.querySelector('.switch p').innerHTML;
         if(loginFormId.style.display === 'none') {
             content = "Already have an account? <a href='#' id='switchForm'>Login</a>";
         }
